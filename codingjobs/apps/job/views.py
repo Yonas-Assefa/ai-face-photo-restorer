823d3acf56973067ed337fab6468d4bc587ce309
Yonas Assefa <se.yonas.assefa@gmail.com>
2022-03-14 15:13:23 +0200

    job addition feature added

--- codingjobs/apps/job/views.py
+++ codingjobs/apps/job/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+import imp
+from multiprocessing import context
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 from .models import Job
+from .forms import AddJobForm
 
 # Create your views here.
 
@@ -7,3 +11,19 @@ def job_detail(request,job_id):
     job=Job.objects.get(pk=job_id)
     context={'job':job}
     return render(request,'job/job_detail.html',context)
+
+@login_required
+def add_job(request):
+    if request.method=="POST":
+        form=AddJobForm(request.POST)
+
+        if form.is_valid():
+            job=form.save(commit=False)
+            job.created_by=request.user
+            job.save()
+            
+            return redirect('dashboard')
+    else:
+        form=AddJobForm()
+    context={'form':form}
+    return render(request,'job/add_job.html',context)
--- codingjobs/codingjobs/urls.py
+++ codingjobs/codingjobs/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from unicodedata import name
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth  import views
 
 from apps.core.views import frontPage,signup
 from apps.job.views import job_detail
+from apps.job.views import add_job
 from apps.userprofile.views import dashboard
 
 urlpatterns = [
@@ -28,6 +29,9 @@ urlpatterns = [
     path('logout/',views.LogoutView.as_view(),name="logout"),
     path('login/',views.LoginView.as_view(template_name='core/login.html'),name="login"),
     path('admin/', admin.site.urls),
-    path('dashboard/',dashboard,name='dashboard'),
-    path('jobs/<int:job_id>/',job_detail,name="job_detail"),
+    path('dashboard/',include('apps.userprofile.urls')),
+    path('jobs/',include('apps.job.urls')),
+
+    # path('jobs/add/',add_job,name='add_job'), 
+    # path('jobs/<int:job_id>/',job_detail,name="job_detail"),
 ]
