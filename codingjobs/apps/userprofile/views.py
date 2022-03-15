f63592c9ebb2ea3e9b0e1a044e16e11e45fc3eca
Yonas Assefa <se.yonas.assefa@gmail.com>
2022-03-15 10:40:13 +0200

    comminication message between the employer and job seeker made

--- codingjobs/apps/userprofile/views.py
+++ codingjobs/apps/userprofile/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
-from apps.job.models import Application
+from apps.job.models import Application,Job
+from apps.userprofile.models import ConversationMessage
+
 
 # Create your views here.
 @login_required
@@ -8,7 +10,24 @@ def dashboard(request):
     context={'userprofile':request.user.userprofile}
     return render(request,'userprofile/dashboard.html',context)
 
+@login_required
 def view_application(request,application_id):
-    application=get_object_or_404(Application,pk=application_id,created_by=request.user)
-    context={'application':application}
+    if request.user.userprofile.is_employer:
+        application=get_object_or_404(Application,pk=application_id,job__created_by=request.user)
+    else:
+        application=get_object_or_404(Application,pk=application_id,created_by=request.user)
+    if request.method=='POST':
+        content=request.POST.get('content')
+        if content:
+            conversationmessage=ConversationMessage.objects.create(application=application, content=content,created_by=request.user)
+            return redirect('view_application',application_id= application_id)
+  
+    context={'application':application,}
     return render(request,'userprofile/view_application.html',context)
+
+@login_required
+def view_dashboard_job(request,job_id):
+    job=get_object_or_404(Job,pk=job_id,created_by=request.user)
+    context={'job':job}
+
+    return render(request,'userprofile/view_dashboard_job.html',context)
